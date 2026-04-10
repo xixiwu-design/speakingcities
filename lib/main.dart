88834091ef36bce2a93fcc993bf0adc9c178e32b
Juju-566 <judit_garrido@ceroca.cat>
2026-04-10 11:22:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       home: Scaffold(
         body: Center(
           child: Text('Hello World!a'),
