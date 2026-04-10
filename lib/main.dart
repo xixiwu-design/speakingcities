e28fdf929208ede0924f1afe51e9bb54b04c3f85
Juju-566 <judit_garrido@ceroca.cat>
2026-04-10 11:25:11 +0200

    cinta debug

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Fotofunete(URL: "lib/images/foto1.jpg"),
