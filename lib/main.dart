997039ad52177fcaba8307c189cdf4b89162a28a
Juju-566 <judit_garrido@ceroca.cat>
2026-04-13 10:51:09 +0200

    barra1

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,46 @@ class MainApp extends StatelessWidget {
         body: Container(
           child: Stack(
             children: [
+              SizedBox(child: Fotofunete(URL: "lib/images/foto5.jpg")),
 
-             
-                SizedBox(child: Fotofunete(URL: "lib/images/foto5.jpg")),
+              Center(child: SetFoto(url: "lib/images/foto4.jpg", height: 200)),
               
-
-
-               Center(
-                 child: SetFoto(
-                    url: "lib/images/foto4.jpg",
-                    height: 200,
+              
+              
+              Container(
+                height: 50,
+                width: double.infinity,               
+                color: Color.fromARGB(146, 30, 52, 150),
+                child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Icon(Icons.account_box_sharp),
                   ),
-               ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(188, 12, 23, 75),
+                        border: Border.all(
+                          color: Color.fromARGB(146, 209, 213, 231),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ),
               
-
             ],
           ),
         ),
-          ),
-      );
-    
+      ),
+    );
   }
 }
