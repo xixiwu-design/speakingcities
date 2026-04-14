f2314c5cc3affc0d9ee2479078abbba7019dfdbb
Juju-566 <judit_garrido@ceroca.cat>
2026-04-14 13:36:14 +0200

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:speakingcities/components/FotoFunete.dart';
 import 'package:speakingcities/components/SetFoto.dart';
+import 'package:speakingcities/components/Titulo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,7 +14,7 @@ class HomePage extends StatelessWidget {
             children: [
               SizedBox(child: Fotofunete(URL: "lib/images/foto5.jpg")),
               Center(child: SetFoto(url: "lib/images/foto4.jpg", height: 200)),
-                        
+                        Titulo(text: "Sydney"),
               Container(
                 height: 50,
                 width: double.infinity,
